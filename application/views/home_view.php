3a8b79601d9d5b0931523fe72496f7410ef006f3
sb727 <user4@mail.com>
2016-03-15 17:44:48 -0700

    home_view.php modified

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,7 +4,7 @@
    <title>Simple Login with CodeIgniter - Private Area</title>
  </head>
  <body>
-   <h1>Home</h1>
+   <h1>Chaged Home</h1>
    <h2>Welcome <?php echo $username; ?>!</h2>
    <a href="home/logout">Logout</a>
  </body>
